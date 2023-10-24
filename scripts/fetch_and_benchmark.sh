65b1f7767f731dcaad1f11a17d04508c0d0555bd
David Rose <david010@gmail.com>
2023-10-24 14:34:03 -0500

    add filter for model size

--- scripts/fetch_and_benchmark.sh
+++ scripts/fetch_and_benchmark.sh
@@ -1,17 +1,58 @@
 #!/bin/bash
 
-declare -A modelStatus
+# Fetch models using curl and jq
+models=$(curl -s -G "https://huggingface.co/api/models" \
+    --data-urlencode "sort=downloads" \
+    --data-urlencode "direction=-1" \
+    --data-urlencode "limit=100" \
+    --data-urlencode "filter=text-generation" | jq -r '.[].id')
 
-# models=$(curl -s -G "https://huggingface.co/api/models" \
-#     --data-urlencode "sort=downloads" \
-#     --data-urlencode "direction=-1" \
-#     --data-urlencode "limit=10" \
-#     --data-urlencode "filter=text-generation" | jq -r '.[].id')
+# Explicitly split by newline into an array
+modelArray=($(echo "$models" | tr '\n' ' '))
 
-models="bigscience/bloom-560m gpt2 EleutherAI/pythia-1.4b"
+# Initialize an empty array to store valid models
+validModels=()
+
+# Initialize an empty array to store dropped models
+droppedModels=()
+
+# Loop to filter out the models
+for model in "${modelArray[@]}"; do
+  echo "Debug: Processing model -> $model"  # Debug print
+
+  # Extract parameter count substring
+  paramCount=$(echo "$model" | sed -nE 's/.*-([0-9]+[MmBb]).*/\1/p')
+  echo "Debug: Extracted param count -> $paramCount"  # Debug print
+
+  # If parameter count is empty, include it
+  if [ -z "$paramCount" ]; then
+    validModels+=("$model")
+    continue
+  fi
+
+  # If it's in billions, check the numerical part
+  if [[ ${paramCount: -1} == 'B' || ${paramCount: -1} == 'b' ]]; then
+    numericalPart=${paramCount:0:${#paramCount}-1}
+    if (( numericalPart > 5 )); then
+      droppedModels+=("$model")
+      continue
+    fi
+  fi
+
+  # If it passed all checks, add it to the valid models list
+  validModels+=("$model")
+done
+
+# Convert the valid model array back to a string, using space as a separator
+IFS=' '
+filteredModels="${validModels[*]}"
+droppedModelsString="${droppedModels[*]}"
+
+echo "Filtered models (Kept): $filteredModels"
+echo "Filtered models (Dropped): $droppedModelsString"
 
 
-for model in $models; do
+for model in $filteredModels; do
     # Encode the forward slashes in the model name
     encodedModel=$(echo "$model" | sed 's/\//%2F/g')
 
