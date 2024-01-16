2af157503674e6f6f9220c7183c3c837b3957e7e
David Rose <david010@gmail.com>
2024-01-16 15:15:43 -0600

    update gguf model grab

--- scripts/run_gguf.py
+++ scripts/run_gguf.py
@@ -1,8 +1,8 @@
+import os
 from typing import Dict
 
 import click
 import requests
-
 
 FLASK_PORT = 5003
 
@@ -12,17 +12,11 @@ FLASK_PORT = 5003
 def bench_gguf(limit: int) -> None:
     """Benchmark all models on the gguf server."""
 
-    model_names = [
-        "llama-3B",
-        "llama-7B",
-        "llama-13B",
-    ]
-    quant_types = [
-        "/ggml-model-q4_0.gguf",
-        "/ggml-model-q8_0.gguf",
-        "/ggml-model-f16.gguf",
-    ]
+    # Get all models and quant types from disk
+    model_dir = "/gemini/gguf"
+    model_names, quant_types = get_models_and_quant_types(model_dir)
 
+    # Run benchmarks
     model_status: Dict[str, int] = {}
     stop = False
     for model in model_names:
@@ -54,6 +48,36 @@ def bench_gguf(limit: int) -> None:
     for model, code in model_status.items():
         print(f"Model: {model}, HTTP Response Code: {code} {'✅' if code == 200 else '❌'}")
     print("🎊 Done 🎊")
+
+
+def get_quant_type(file: str) -> str:
+    """Get quantization type from file name."""
+    if "f16" in file:
+        return "f16"
+    elif "int8" in file:
+        return "8bit"
+    elif "int4" in file:
+        return "4bit"
+    else:
+        raise ValueError(f"Unknown quant type for file: {file}")
+
+
+def get_models_and_quant_types(model_dir: str = "/gemini/gguf") -> tuple:
+    """Get list of .gguf models and their quant types from any dirs in model_dir."""
+    model_names = []
+    quant_types = []
+
+    for root, dirs, files in os.walk(model_dir):
+        for file in files:
+            if file.endswith(".gguf"):
+                model_name = os.path.basename(root)
+                model_names.append(model_name)
+                quant_type = get_quant_type(file)
+                quant_types.append(quant_type)
+                print(f"Found model: {model_name} with quant: {quant_type}")
+
+    print(f"Found {len(model_names)} models.")
+    return model_names, quant_types
 
 
 if __name__ == "__main__":
