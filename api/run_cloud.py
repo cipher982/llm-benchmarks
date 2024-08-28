9eb4ed9c71dd8fd04ff9888f795661f324e20fd0
David Rose <david010@gmail.com>
2024-08-28 09:01:53 -0500

    use fewer tokens

--- api/run_cloud.py
+++ api/run_cloud.py
@@ -29,7 +29,7 @@ logger = Logger(
 
 # Constants
 QUERY_TEXT = "Tell a long and happy story about the history of the world."
-MAX_TOKENS = 128
+MAX_TOKENS = 64
 TEMPERATURE = 0.1
 FASTAPI_PORT = os.environ.get("FASTAPI_PORT_CLOUD")
 assert FASTAPI_PORT, "FASTAPI_PORT environment variable not set"
